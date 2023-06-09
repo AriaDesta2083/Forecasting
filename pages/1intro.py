29545fb9c1aae146dccccd8dfbf650359a1e9ed8
Ar <rejoagung2223@gmail.com>
2023-06-10 02:56:24 +0700

    update - intro : masih ide. - plotting: membuat chart data hari dan data bulan. - dataframe: menampilkan dataframe harga perhari dan perbulan , membuat fungsi mencari data berdasarkan harga dan tanggal.

---
+++ pages/1intro.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Intro", page_icon="📈")
+st.title('FORECASTING 📈')
+st.write('Halaman ini akan menjeleskan secara ringkas apa itu forecasting dan bagaimana pengimplementasian forecasting di lakukan')
